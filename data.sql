b9cc4bde461f6288ae749f16823514623ed6a093
Mahdi Mahdawi <59498020+mahdimahdawi@users.noreply.github.com>
2023-01-17 15:18:57 +0430

    First version

--- data.sql
+++ data.sql
@@ -1,5 +1,6 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+insert into animals values(1, 'Agumon', '2020-feb-3', 0, True, 10.23);
+insert into animals values(2, 'Gabumon', '2018-nov-15', 2, True, 80);
+insert into animals values(3, 'Pikachu', '2021-jan-7', 1, False, 15.04);
+insert into animals values(4, 'Devimon', '2017-may-12', 5, True, 11);
--- queries.sql
+++ queries.sql
@@ -1,3 +1,10 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name = 'Luna';
+select * from animals where name like '%mon';
+select name from animals where date_of_birth between '2016-1-1' and '2019-1-1';
+select name from animals where nutered = True and escape_attempts < 3;
+select date_of_birth from animals where name='Agumon' or name = 'Pikachu';
+select name, escape_attempts from animals where weight_kg > 10.5;
+select * from animals where nutered = True;
+select * from animals where name != 'Gabumon';
+select * from animals where weight_kg <= 17.3 and weight_kg >= 10.4;
